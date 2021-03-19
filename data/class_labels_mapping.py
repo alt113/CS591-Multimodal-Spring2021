dfed1ced780ae3fce02b7256cf0aec867b75673a
Rayyan Nasr <developer.rayyan.nasr@gmail.com>
2021-03-19 09:39:09 +0200

    added helper file in the data directory to perform class string label name to integer mapping

---
+++ data/class_labels_mapping.py
@@ -0,0 +1,50 @@
+"""
+    This file contains the mapping of the class labels present in the 'single' folder of the
+    FAT dataset to the corresponding integers in order to perform downstream classification.
+    Below is the corresponding class label/integer mapping:
+
+    - 002_master_chef_can_16k: 0
+    - 003_cracker_box_16k: 1
+    - 004_sugar_box_16k: 2
+    - 005_tomato_soup_can_16k: 3
+    - 006_mustard_bottle_16k: 4
+    - 007_tuna_fish_can_16k: 5
+    - 008_pudding_box_16k: 6
+    - 009_gelatin_box_16k: 7
+    - 010_potted_meat_can_16k: 8
+    - 011_banana_16k: 9
+    - 019_pitcher_base_16k: 10
+    - 021_bleach_cleanser_16k: 11
+    - 024_bowl_16k: 12
+    - 025_mug_16k: 13
+    - 035_power_drill_16k: 14
+    - 036_wood_block_16k: 15
+    - 037_scissors_16k: 16
+    - 040_large_marker_16k: 17
+    - 051_large_clamp_16k: 18
+    - 052_extra_large_clamp_16k: 19
+    - 061_foam_brick_16k: 20
+"""
+class_label_map = {
+    '002_master_chef_can_16k': 0,
+    '003_cracker_box_16k': 1,
+    '004_sugar_box_16k': 2,
+    '005_tomato_soup_can_16k': 3,
+    '006_mustard_bottle_16k': 4,
+    '007_tuna_fish_can_16k': 5,
+    '008_pudding_box_16k': 6,
+    '009_gelatin_box_16k': 7,
+    '010_potted_meat_can_16k': 8,
+    '011_banana_16k': 9,
+    '019_pitcher_base_16k': 10,
+    '021_bleach_cleanser_16k': 11,
+    '024_bowl_16k': 12,
+    '025_mug_16k': 13,
+    '035_power_drill_16k': 14,
+    '036_wood_block_16k': 15,
+    '037_scissors_16k': 16,
+    '040_large_marker_16k': 17,
+    '051_large_clamp_16k': 18,
+    '052_extra_large_clamp_16k': 19,
+    '061_foam_brick_16k': 20
+}
