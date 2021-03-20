fcb7eaacafb572b80524f46673094325d9f970ea
Rayyan Nasr <developer.rayyan.nasr@gmail.com>
2021-03-20 14:59:49 +0200

    added class label mapping function for tf.Dataset generator

--- data/data_tf.py
+++ data/data_tf.py
@@ -1,14 +1,3 @@
-Show
-Dotfiles
-Show
-Owner / Mode
-/ projectnb / cs591 - mm - ml / rnasr /
-# !/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import tensorflow as tf
 import numpy as np
 
@@ -16,11 +5,52 @@ import os
 from PIL import Image
 import orjson
 
-import matplotlib.pyplot as plt
-
-# In[54]:
-
 data_path = '/projectnb/cs591-mm-ml/prichter/single'
+
+
+def map_class_labels(rgb, depth, label):
+    if label == '002_master_chef_can_16k':
+        return rgb, depth, '0'
+    elif label == '003_cracker_box_16k':
+        return rgb, depth, '1'
+    elif label == '004_sugar_box_16k':
+        return rgb, depth, '2'
+    elif label == '005_tomato_soup_can_16k':
+        return rgb, depth, '3'
+    elif label == '006_mustard_bottle_16k':
+        return rgb, depth, '4'
+    elif label == '007_tuna_fish_can_16k':
+        return rgb, depth, '5'
+    elif label == '008_pudding_box_16k':
+        return rgb, depth, '6'
+    elif label == '009_gelatin_box_16k':
+        return rgb, depth, '7'
+    elif label == '010_potted_meat_can_16k':
+        return rgb, depth, '8'
+    elif label == '011_banana_16k':
+        return rgb, depth, '9'
+    elif label == '019_pitcher_base_16k':
+        return rgb, depth, '10'
+    elif label == '021_bleach_cleanser_16k':
+        return rgb, depth, '11'
+    elif label == '024_bowl_16k':
+        return rgb, depth, '12'
+    elif label == '025_mug_16k':
+        return rgb, depth, '13'
+    elif label == '035_power_drill_16k':
+        return rgb, depth, '14'
+    elif label == '036_wood_block_16k':
+        return rgb, depth, '15'
+    elif label == '037_scissors_16k':
+        return rgb, depth, '16'
+    elif label == '040_large_marker_16k':
+        return rgb, depth, '17'
+    elif label == '051_large_clamp_16k':
+        return rgb, depth, '18'
+    elif label == '052_extra_large_clamp_16k':
+        return rgb, depth, '19'
+    elif label == '061_foam_brick_16k':
+        return rgb, depth, '20'
 
 
 def load_image(data_path, box):
@@ -76,24 +106,19 @@ def sample_generator(split='train'):
         yield rgb_image, depth_image, c
 
 
-# In[55]:
-
-
 ds = tf.data.Dataset.from_generator(
     sample_generator,
     (tf.int32, tf.int32, tf.string),
     ([128, 128, 3], [128, 128, 3], [])
 )
 
-# In[56]:
-
+ds = ds.map(map_class_labels)
 
 for image, depth, label in ds.take(1):
     i = image
     d = depth
-
-# In[57]:
-
+    l = label
 
 print(i.shape)
 print(d.shape)
+print(l)
